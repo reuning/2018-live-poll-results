ce9702000d24a4aac8031755a0d25ac9672057de
Reuning <reunink@miamioh.edu>
2018-11-01 12:13:54 -0400

    Finalish

--- AV_Estimate.R
+++ AV_Estimate.R
@@ -295,10 +295,6 @@ dev.off()
 
 
 df$response <- ordered(df$response, c("Dem", "Und", "Rep"))
-mod <- stan_polr(response~age_combined + educ + 
-              race_gender  + partyid + stdist, data=df, subset = voted==1, 
-              method="logistic", prior=NULL)
-
 
 
 library(brms)
@@ -306,10 +302,7 @@ library(brms)
 mod3 <- brm(response~(1 + partyid|age_combined) + (1 + partyid|educ) + 
                         (1+partyid|race_gender)  +
                         (1|partyid) + (1|stdist), data=df[df$voted==1,],
-            family=categorical(), chains=2, cores=2)
-
-
-
+            family=categorical(), chains=2, cores=2, control=list("adapt_delta"=.99, "max_treedepth"=15))
 
 
 
@@ -328,7 +321,7 @@ pred.df <-unique(pred.df)
 
 
 
-out <- replicate(100, apply(predict(mod3, newdata=pred.df,
+out3 <- replicate(100, apply(predict(mod3, newdata=pred.df,
                                      allow_new_levels=T, summary=F, 
                                      nsamples = 100), 2, function(x){
                                        c(sum(x==1), sum(x==2), sum(x==3))
@@ -336,12 +329,11 @@ out <- replicate(100, apply(predict(mod3, newdata=pred.df,
 
 library(abind)
 
-array(t(unlist(out)), c(3,3,100))
 
-tmp <- apply(out, c(1,2), median)
-lo <- apply(out, c(1,2), quantile, p=0.05)
+tmp <- apply(out3, c(1,2), median)
+lo <- apply(out3, c(1,2), quantile, p=0.05)
 lo <- c(lo)
-up <- apply(out, c(1,2), quantile, p=0.95)
+up <- apply(out3, c(1,2), quantile, p=0.95)
 up <- c(up)
 
 colnames(tmp) <- c("Republicans", "Independents", "Democrats")
@@ -364,5 +356,117 @@ for(ii in 1:length(up)){
 
 dev.off()
 
+
+
+mod4 <- brm(response~(1 + file_party|age_combined) + (1 + file_party|educ) + 
+              (1+file_party|race_gender)  +
+              (1|file_party) + (1|stdist), data=df[df$voted==1,],
+            family=categorical(), chains=2, cores=2, control=list("adapt_delta"=.99,  "max_treedepth"=15))
+
+
+
+
+
+pred.df <- df[df$voted==1,c("age_combined", "educ", "race_gender",  "file_party")]
+pred.df <- pred.df[complete.cases(pred.df),]
+pred.df <- unique(pred.df)
+pred.df$stdist <- "New"
+pred.df$educ <- "New"
+pred.df$age_combined <- "New"
+pred.df$race_gender <- "New"
+# pred.df$response <- "New"
+
+pred.df <-unique(pred.df)
+
+
+
+out4 <- replicate(100, apply(predict(mod4, newdata=pred.df,
+                                    allow_new_levels=T, summary=F, 
+                                    nsamples = 100), 2, function(x){
+                                      c(sum(x==1), sum(x==2), sum(x==3))
+                                    }))
+
+
+
+tmp <- apply(out4, c(1,2), median)
+lo <- apply(out4, c(1,2), quantile, p=0.05)
+lo <- c(lo)
+up <- apply(out4, c(1,2), quantile, p=0.95)
+up <- c(up)
+
+pct <- by(df$final_weight[df$voted==1], df$file_party[df$voted==1], sum)/sum(df$final_weight[df$voted==1], na.rm=T)
+
+colnames(tmp) <- paste(c("Republicans", "Other", "Democrats"),
+                       "\n",round(pct[c("Republican", "Other", "Democratic")]*100,2),"%", sep="")
+
+png("Indep_Vote_Reg.png", height=8, width=8, 
+    units="in", res=200)
+par(family="mstreg")
+barplot(tmp, beside = T, col=scales::alpha(c( "#124073","#A8BF14", "#B71D1A"), .6), 
+        yaxt="n", xlim=c(1,15), ylim=c(0,100), border=NA)
+axis(2, family="mstthin")
+legend("right", legend=c("Democrat", "Undecided", "Republican"), 
+       fill=scales::alpha(c( "#124073","#A8BF14", "#B71D1A"), .6), 
+       title="Vote Choice", bty="n", border=NA)
+title(main=toupper("Independent Early Voters are\nBreaking towards Democrats"), family="futura")
+title(ylab="Percentage", xlab="Voter File")
+for(ii in 1:length(up)){
+  lines(x=c(ii, ii)+(.5 + ((ii -1) %/% 3)), y=c(lo[ii], up[ii]), 
+        lwd=2, col=c("#B71D1A", "#124073","#A8BF14")[((ii %% 3) + 1)])
+}
+
+dev.off()
+
+
+library(abind)
+
+
+tmp <- apply(out4 - out3, c(1,2), median)
+tmp <- c(tmp)
+lo <- apply(out4 - out3, c(1,2), quantile, p=0.05)
+lo <- c(lo)
+lo.mid <- apply(out4 - out3, c(1,2), quantile, p=0.10)
+lo.mid <- c(lo.mid)
+
+up <- apply(out4 - out3, c(1,2), quantile, p=0.95)
+up <- c(up)
+up.mid <- apply(out4 - out3, c(1,2), quantile, p=0.90)
+up.mid <- c(up.mid)
+
+plot.df <- data.frame(median=tmp, lo=lo, up=up,up.mid=up.mid, lo.mid=lo.mid,
+                      group=c(rep("Republicans", 3),
+                              rep("Other", 3), rep("Democrat", 3)), 
+                      choice=rep(c("Democrat", "Undecided", "Republican"),3))
+  
+png("Vote_Compare.png", height=8, width=8, 
+    units="in", res=200)
+
+
+ggplot(plot.df) + geom_pointrange(aes(ymin=lo.mid, ymax=up.mid, y=median, 
+                                      x=group,  color=choice ), 
+                                  position=position_dodge(width=.5)) +
+  geom_linerange(aes(ymin=lo, ymax=up, x=group,  color=choice), 
+                 position=position_dodge(width=.5), lwd=.2) + 
+  coord_flip() + ylab("Difference in Model Predictions") +xlab("") + 
+  annotate("text", label=paste(expression(""%<-%"Under Reporting")), x=.5, y=-8,  parse=T) + 
+  annotate("text", label=paste(expression("Over Reporting"%->%"")), x=.5, y=8,  parse=T) + 
+  ggtitle(toupper("voter file partisanship under-predicts support")) + 
+  scale_colour_manual(values = c( "#124073","#B71D1A", "#A8BF14"), 
+                      name="Vote Choice") + theme_classic(base_size=14) +
+  theme(plot.title=element_text(family="futura", hjust=.5), 
+        text=element_text(family="mstreg"), 
+        panel.grid=element_line(size=0), 
+        panel.grid.major.y=element_line(size=.5, color=alpha("gray", .5), 
+                                        linetype = 2), 
+        panel.border=element_rect(size=0, fill=NA), 
+        axis.text.x = element_text(family="mstthin", color="black"))
+
+dev.off()
+
+pp4 <- posterior_predict(mod4)
+pp3 <- posterior_predict(mod3)
+
+pp3.est <- apply(pp3, 1, function(x) sum(diag(table(x, as.numeric(mod3$data$response)))))/length(mod3$data$response)
+pp4.est <- apply(pp4, 1, function(x) sum(diag(table(x, as.numeric(mod4$data$response)))))/length(mod4$data$response)
 
 save.image("Out.RData")
